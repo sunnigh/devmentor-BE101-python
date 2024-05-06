5e8c8df85bbd8b47d84c6ef35c2a420d35001cae
sunnigh <d7031887@gmail.com>
2024-05-06 19:20:42 +0800

    user/do

--- user.py
+++ user.py
@@ -1,11 +1,18 @@
+import enus
 from language_system import LanguageSystem
 
 
 class User:
     language: LanguageSystem
 
-    def __init__(self, lang: LanguageSystem):
+    def __init__(self, username: str, lang: LanguageSystem):
         self.language = lang
+        self.username = username
+
+    def do(self):
+        print(self.username)
 
 
-
+language = enus.Enus()
+user = User("Alex", language)
+user.do()
